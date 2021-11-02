863ed83f13bc5f4382f718628919a6dc0ac7cd5c
Saecki <tobiasschmitz2001@gmail.com>
2021-11-02 02:14:59 +0100

    convert crates/types to teal

--- lua/crates/types.lua
+++ lua/crates/types.lua
@@ -1,55 +1,40 @@
----@class Range
----@field s integer -- 0-indexed inclusive
----@field e integer -- 0-indexed exclusive
+local M = {Range = {}, }
 
-local M = {}
 
----@type Range
-M.Range = {}
+
+
+
+
 local Range = M.Range
 
----@param s integer
----@param e integer
----@return Range
 function Range.new(s, e)
-    return setmetatable({ s = s, e = e }, { __index = Range })
+   return setmetatable({ s = s, e = e }, { __index = Range })
 end
 
----@param p integer
----@return Range
 function Range.pos(p)
-    return Range.new(p, p + 1)
+   return Range.new(p, p + 1)
 end
 
----@param self Range
----@param pos integer
----@return boolean
 function Range:contains(pos)
-    return self.s <= pos and pos <  self.e
+   return self.s <= pos and pos < self.e
 end
 
--- Create a new range with moved start and end bounds
----@param self Range
----@param s integer
----@param e integer
----@return Range
+
 function Range:moved(s, e)
-    return Range.new(self.s + s, self.e + e)
+   return Range.new(self.s + s, self.e + e)
 end
 
----@param self Range
----@return fun(): integer
 function Range:iter()
-    local i = self.s
-    return function()
-        if i >= self.e then
-            return nil
-        end
+   local i = self.s
+   return function()
+      if i >= self.e then
+         return nil
+      end
 
-        local val = i
-        i = i + 1
-        return val
-    end
+      local val = i
+      i = i + 1
+      return val
+   end
 end
 
 return M
---
+++ tlconfig.lua
@@ -0,0 +1,7 @@
+return {
+  gen_target = "5.1",
+  gen_compat = "off",
+  include_dir = { "teal" },
+  source_dir = "teal",
+  build_dir = "lua",
+}
