a745805bc0e38693f9427319cffba75fd7c916db
Saecki <tobiasschmitz2001@gmail.com>
2021-11-03 09:40:54 +0100

    convert crates/time to teal

--- lua/crates/time.lua
+++ lua/crates/time.lua
@@ -1,53 +1,46 @@
----@class DateTime
----@field epoch integer
+local M = {DateTime = {}, }
 
-local M = {}
 
----@type DateTime
-M.DateTime = {}
+
+
+
 local DateTime = M.DateTime
 
----@param epoch integer
----@return DateTime
 function DateTime.new(epoch)
-    return setmetatable({ epoch = epoch }, { __index = DateTime })
+   return setmetatable({ epoch = epoch }, { __index = DateTime })
 end
 
----@param str string
----@return DateTime
 function DateTime.parse_rfc_3339(str)
-    -- lua regex suports no {n} occurences
-    local pat = "^([0-9][0-9][0-9][0-9])%-([0-9][0-9])%-([0-9][0-9])" -- date
-    .."T([0-9][0-9]):([0-9][0-9]):([0-9][0-9])%.[0-9]+" -- time
-    .."([%+%-])([0-9][0-9]):([0-9][0-9])$" -- offset
 
-    local year, month, day, hour, minute, second, offset, offset_hour, offset_minute = str:match(pat)
-    if year then
-        if offset == "+" then
-            hour = hour + offset_hour
-            minute = minute + offset_minute
-        elseif offset == "-" then
-            hour = hour - offset_hour
-            minute = minute - offset_minute
-        end
-        return DateTime.new(os.time {
-            year = tonumber(year),
-            month = tonumber(month),
-            day = tonumber(day),
-            hour = tonumber(hour),
-            minute = tonumber(minute),
-            second = tonumber(second),
-        })
-    end
+   local pat = "^([0-9][0-9][0-9][0-9])%-([0-9][0-9])%-([0-9][0-9])" ..
+   "T([0-9][0-9]):([0-9][0-9]):([0-9][0-9])%.[0-9]+" ..
+   "([%+%-])([0-9][0-9]):([0-9][0-9])$"
 
-    return nil
+   local year, month, day, hour, minute, second, offset, offset_hour, offset_minute = str:match(pat)
+   if year then
+      local h, m
+      if offset == "+" then
+         h = tonumber(hour) + tonumber(offset_hour)
+         m = tonumber(minute) + tonumber(offset_minute)
+      elseif offset == "-" then
+         h = tonumber(hour) - tonumber(offset_hour)
+         m = tonumber(minute) - tonumber(offset_minute)
+      end
+      return DateTime.new(os.time({
+         year = tonumber(year),
+         month = tonumber(month),
+         day = tonumber(day),
+         hour = h,
+         minute = m,
+         second = tonumber(second),
+      }))
+   end
+
+   return nil
 end
 
----@param self DateTime
----@param format string
----@return string
 function DateTime:display(format)
-    return os.date(format, self.epoch)
+   return os.date(format, self.epoch)
 end
 
 return M
