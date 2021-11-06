b61a4e1e5e5a43f6607415cc2ac0c0604267323b
Saecki <tobiasschmitz2001@gmail.com>
2021-11-06 13:02:38 +0100

    convert crates to teal

--- lua/crates.lua
+++ lua/crates.lua
@@ -1,159 +1,159 @@
 local M = {}
 
+
+
+
+
+
+
 local api = require('crates.api')
-local config = require('crates.config')
+local Version = api.Version
+local Config = require('crates.config')
 local core = require('crates.core')
 local popup = require('crates.popup')
 local toml = require('crates.toml')
+local Crate = toml.Crate
 local util = require('crates.util')
 local ui = require('crates.ui')
 local Range = require('crates.types').Range
 
----@param crate Crate
 function M.reload_crate(crate)
-    local function on_fetched(versions)
-        if versions and versions[1] then
-            core.vers_cache[crate.name] = versions
-        end
+   local function on_fetched(versions)
+      if versions and versions[1] then
+         core.vers_cache[crate.name] = versions
+      end
 
-        for buf,crates in pairs(core.crate_cache) do
-            local c = crates[crate.name]
+      for buf, crates in pairs(core.crate_cache) do
+         local c = crates[crate.name]
 
-            -- only update loaded buffers
-            if c and vim.api.nvim_buf_is_loaded(buf) then
-                ui.display_versions(buf, c, versions)
-            end
-        end
-    end
 
-    if core.cfg.loading_indicator then
-        ui.display_loading(0, crate)
-    end
+         if c and vim.api.nvim_buf_is_loaded(buf) then
+            ui.display_versions(buf, c, versions)
+         end
+      end
+   end
 
-    api.fetch_crate_versions(crate.name, on_fetched)
+   if core.cfg.loading_indicator then
+      ui.display_loading(0, crate)
+   end
+
+   api.fetch_crate_versions(crate.name, on_fetched)
 end
 
 function M.hide()
-    core.visible = false
-    ui.clear()
+   core.visible = false
+   ui.clear()
 end
 
 function M.reload()
-    core.visible = true
-    core.vers_cache = {}
-    ui.clear()
+   core.visible = true
+   core.vers_cache = {}
+   ui.clear()
 
-    local cur_buf = util.current_buf()
-    local crates = toml.parse_crates(0)
+   local cur_buf = util.current_buf()
+   local crates = toml.parse_crates(0)
 
-    core.crate_cache[cur_buf] = {}
+   core.crate_cache[cur_buf] = {}
 
-    for _,c in ipairs(crates) do
-        core.crate_cache[cur_buf][c.name] = c
-        M.reload_crate(c)
-    end
+   for _, c in ipairs(crates) do
+      core.crate_cache[cur_buf][c.name] = c
+      M.reload_crate(c)
+   end
 end
 
 function M.update()
-    core.visible = true
-    ui.clear()
+   core.visible = true
+   ui.clear()
 
-    local cur_buf = util.current_buf()
-    local crates = toml.parse_crates(0)
+   local cur_buf = util.current_buf()
+   local crates = toml.parse_crates(0)
 
-    core.crate_cache[cur_buf] = {}
+   core.crate_cache[cur_buf] = {}
 
-    for _,c in ipairs(crates) do
-        local versions = core.vers_cache[c.name]
+   for _, c in ipairs(crates) do
+      local versions = core.vers_cache[c.name]
 
-        core.crate_cache[cur_buf][c.name] = c
+      core.crate_cache[cur_buf][c.name] = c
 
-        if versions then
-            ui.display_versions(0, c, versions)
-        else
-            M.reload_crate(c)
-        end
-    end
+      if versions then
+         ui.display_versions(0, c, versions)
+      else
+         M.reload_crate(c)
+      end
+   end
 end
 
 function M.toggle()
-    if core.visible then
-        M.hide()
-    else
-        M.update()
-    end
+   if core.visible then
+      M.hide()
+   else
+      M.update()
+   end
 end
 
 
---- upgrade the crate on the current line
----@param smart boolean | nil
+
 function M.upgrade_crate(smart)
-    local linenr = vim.api.nvim_win_get_cursor(0)[1]
-    util.upgrade_crates(Range.new(linenr - 1, linenr ), smart)
+   local linenr = vim.api.nvim_win_get_cursor(0)[1]
+   util.upgrade_crates(Range.pos(linenr - 1), smart)
 end
 
---- upgrade the crates on the lines visually selected
----@param smart boolean | nil
+
 function M.upgrade_crates(smart)
-    local lines = Range.new(
-        vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
-        vim.api.nvim_buf_get_mark(0, ">")[1]
-    )
-    util.upgrade_crates(lines, smart)
+   local lines = Range.new(
+   vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
+   vim.api.nvim_buf_get_mark(0, ">")[1])
+
+   util.upgrade_crates(lines, smart)
 end
 
---- upgrade all crates in the buffer
----@param smart boolean | nil
+
 function M.upgrade_all_crates(smart)
-    local lines = Range.new(0, vim.api.nvim_buf_line_count(0))
-    util.upgrade_crates(lines, smart)
+   local lines = Range.new(0, vim.api.nvim_buf_line_count(0))
+   util.upgrade_crates(lines, smart)
 end
 
--- update the crate on the current line
----@param smart boolean | nil
+
 function M.update_crate(smart)
-    local linenr = vim.api.nvim_win_get_cursor(0)[1]
-    util.update_crates(Range.new(linenr - 1, linenr), smart)
+   local linenr = vim.api.nvim_win_get_cursor(0)[1]
+   util.update_crates(Range.pos(linenr - 1), smart)
 end
 
--- update the crates on the lines visually selected
----@param smart boolean | nil
+
 function M.update_crates(smart)
-    local lines = Range.new(
-        vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
-        vim.api.nvim_buf_get_mark(0, ">")[1]
-    )
-    util.update_crates(lines, smart)
+   local lines = Range.new(
+   vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
+   vim.api.nvim_buf_get_mark(0, ">")[1])
+
+   util.update_crates(lines, smart)
 end
 
---- update all crates in the buffer
----@param smart boolean | nil
+
 function M.update_all_crates(smart)
-    local lines = Range.new(0, vim.api.nvim_buf_line_count(0))
-    util.update_crates(lines, smart)
+   local lines = Range.new(0, vim.api.nvim_buf_line_count(0))
+   util.update_crates(lines, smart)
 end
 
 
----@param cfg Config
 function M.setup(cfg)
-    local default = config.default()
-    if cfg then
-        core.cfg = vim.tbl_deep_extend("keep", cfg, default)
-    else
-        core.cfg = vim.tbl_deep_extend("keep", core.cfg, default)
-    end
+   local default = Config.default()
+   if cfg then
+      core.cfg = vim.tbl_deep_extend("keep", cfg, default)
+   else
+      core.cfg = vim.tbl_deep_extend("keep", core.cfg, default)
+   end
 
-    vim.cmd("augroup Crates")
-    vim.cmd("autocmd!")
-    if core.cfg.autoload then
-        vim.cmd("autocmd BufRead Cargo.toml lua require('crates').update()")
-    end
-    if core.cfg.autoupdate then
-        vim.cmd("autocmd TextChanged,TextChangedI,TextChangedP Cargo.toml lua require('crates').update()")
-    end
-    vim.cmd("augroup END")
+   vim.cmd("augroup Crates")
+   vim.cmd("autocmd!")
+   if core.cfg.autoload then
+      vim.cmd("autocmd BufRead Cargo.toml lua require('crates').update()")
+   end
+   if core.cfg.autoupdate then
+      vim.cmd("autocmd TextChanged,TextChangedI,TextChangedP Cargo.toml lua require('crates').update()")
+   end
+   vim.cmd("augroup END")
 
-    vim.cmd([[
+   vim.cmd([[
         augroup CratesPopup
         autocmd!
         autocmd CursorMoved,CursorMovedI Cargo.toml lua require('crates.popup').hide()
