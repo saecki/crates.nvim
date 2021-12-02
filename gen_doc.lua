3d8a2401b7d9d442a65fefb5ed508ae5175cd83b
Saecki <tobiasschmitz2001@gmail.com>
2021-12-02 19:05:03 +0100

    make config schema ordered

--- gen_doc.lua
+++ gen_doc.lua
@@ -17,7 +17,8 @@ local function join_path(path, component)
 end
 
 local function gen_config_doc(lines, path, schema)
-    for k,s in pairs(schema) do
+    for _,s in ipairs(schema) do
+        local k = s.name
         local p = join_path(path, k)
         local key = table.concat(p, ".")
         local doc_key = string.format("*crates-config-%s*", table.concat(p, "-"))
@@ -31,34 +32,32 @@ local function gen_config_doc(lines, path, schema)
 
         if s.deprecated then
             if s.deprecated.new_field then
-                local nf = table.concat(s.deprecated.new_field, ".")
-                table.insert(lines, string.format("    DEPRECATED: please use %s", nf))
+                local nf = "crates-config-" .. table.concat(s.deprecated.new_field, "-")
+                table.insert(lines, "    DEPRECATED")
+                table.insert(lines, "")
+                table.insert(lines, string.format("    Please use %s instead.", nf))
             else
                 table.insert(lines, "    DEPRECATED")
             end
             table.insert(lines, "")
-        end
-
-        if s.type == "section" then
-            table.insert(lines, "    Type: `section`")
-            table.insert(lines, "")
         else
-            local t = s.type
-            if type(t) == "table" then
-                t = table.concat(t, " or ")
+            if s.type == "section" then
+                table.insert(lines, "    Type: `section`")
+                table.insert(lines, "")
+            else
+                local t = s.type
+                if type(t) == "table" then
+                    t = table.concat(t, " or ")
+                end
+                local d = inspect(s.default)
+                table.insert(lines, string.format("    Type: `%s`, Default: `%s`", t, d))
+                table.insert(lines, "")
             end
-            local d = inspect(s.default)
-            table.insert(lines, string.format("    Type: `%s`, Default: `%s`", t, d))
+
+            local description = s.description:gsub("^    ", "")
+            table.insert(lines, description)
             table.insert(lines, "")
         end
-
-        local leading_ws = s.description:match("^%s*()") - 1
-        for l in s.description:gmatch("\n?([^\n]*)\n?") do
-            local line = l:gsub("%s+$", ""):gsub("^" .. string.rep(" ", leading_ws), "    ")
-            table.insert(lines, line)
-        end
-
-        table.insert(lines, "")
 
         if s.type == "section" then
             gen_config_doc(lines, p, s.fields)
