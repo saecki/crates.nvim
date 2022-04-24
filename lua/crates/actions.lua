1a7ac85152fe241e7697503e78cf49744d70a7a9
Saecki <tobiasschmitz2001@gmail.com>
2022-04-24 19:30:56 +0200

    refactor: move actions into separate file

---
+++ lua/crates/actions.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+local util = require("crates.util")
+local state = require("crates.state")
+local types = require("crates.types")
+local Range = types.Range
+
+function M.upgrade_crate(alt)
+   local linenr = vim.api.nvim_win_get_cursor(0)[1]
+   local crates = util.get_lines_crates(Range.pos(linenr - 1))
+   util.upgrade_crates(crates, alt)
+end
+
+function M.upgrade_crates(alt)
+   local lines = Range.new(
+   vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
+   vim.api.nvim_buf_get_mark(0, ">")[1])
+
+   local crates = util.get_lines_crates(lines)
+   util.upgrade_crates(crates, alt)
+end
+
+function M.upgrade_all_crates(alt)
+   local cur_buf = util.current_buf()
+   local crates = state.crate_cache[cur_buf]
+   if not crates then return end
+
+   local crate_versions = {}
+   for _, c in pairs(crates) do
+      table.insert(crate_versions, {
+         crate = c,
+         versions = state.vers_cache[c.name],
+      })
+   end
+
+   util.upgrade_crates(crate_versions, alt)
+end
+
+function M.update_crate(alt)
+   local linenr = vim.api.nvim_win_get_cursor(0)[1]
+   local crates = util.get_lines_crates(Range.pos(linenr - 1))
+   util.update_crates(crates, alt)
+end
+
+function M.update_crates(alt)
+   local lines = Range.new(
+   vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
+   vim.api.nvim_buf_get_mark(0, ">")[1])
+
+   local crates = util.get_lines_crates(lines)
+   util.update_crates(crates, alt)
+end
+
+function M.update_all_crates(alt)
+   local cur_buf = util.current_buf()
+   local crates = state.crate_cache[cur_buf]
+   if not crates then return end
+
+   local crate_versions = {}
+   for _, c in pairs(crates) do
+      table.insert(crate_versions, {
+         crate = c,
+         versions = state.vers_cache[c.name],
+      })
+   end
+
+   util.update_crates(crate_versions, alt)
+end
+
+return M
--- lua/crates/init.lua
+++ lua/crates/init.lua
@@ -50,13 +50,12 @@ local M = {}
 
 
 
+local actions = require("crates.actions")
 local config = require("crates.config")
 local Config = config.Config
 local core = require("crates.core")
 local popup = require("crates.popup")
 local state = require("crates.state")
-local types = require("crates.types")
-local Range = types.Range
 local ui = require("crates.ui")
 local util = require("crates.util")
 
@@ -132,68 +131,12 @@ function M.reload(buf)
    core.update(buf, true)
 end
 
-
-function M.upgrade_crate(alt)
-   local linenr = vim.api.nvim_win_get_cursor(0)[1]
-   local crates = util.get_lines_crates(Range.pos(linenr - 1))
-   util.upgrade_crates(crates, alt)
-end
-
-function M.upgrade_crates(alt)
-   local lines = Range.new(
-   vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
-   vim.api.nvim_buf_get_mark(0, ">")[1])
-
-   local crates = util.get_lines_crates(lines)
-   util.upgrade_crates(crates, alt)
-end
-
-function M.upgrade_all_crates(alt)
-   local cur_buf = util.current_buf()
-   local crates = state.crate_cache[cur_buf]
-   if not crates then return end
-
-   local crate_versions = {}
-   for _, c in pairs(crates) do
-      table.insert(crate_versions, {
-         crate = c,
-         versions = state.vers_cache[c.name],
-      })
-   end
-
-   util.upgrade_crates(crate_versions, alt)
-end
-
-function M.update_crate(alt)
-   local linenr = vim.api.nvim_win_get_cursor(0)[1]
-   local crates = util.get_lines_crates(Range.pos(linenr - 1))
-   util.update_crates(crates, alt)
-end
-
-function M.update_crates(alt)
-   local lines = Range.new(
-   vim.api.nvim_buf_get_mark(0, "<")[1] - 1,
-   vim.api.nvim_buf_get_mark(0, ">")[1])
-
-   local crates = util.get_lines_crates(lines)
-   util.update_crates(crates, alt)
-end
-
-function M.update_all_crates(alt)
-   local cur_buf = util.current_buf()
-   local crates = state.crate_cache[cur_buf]
-   if not crates then return end
-
-   local crate_versions = {}
-   for _, c in pairs(crates) do
-      table.insert(crate_versions, {
-         crate = c,
-         versions = state.vers_cache[c.name],
-      })
-   end
-
-   util.update_crates(crate_versions, alt)
-end
+M.upgrade_crate = actions.upgrade_crate
+M.upgrade_crates = actions.upgrade_crates
+M.upgrade_all_crates = actions.upgrade_all_crates
+M.update_crate = actions.update_crate
+M.update_crates = actions.update_crates
+M.update_all_crates = actions.update_all_crates
 
 M.show_popup = popup.show
 M.show_versions_popup = popup.show_versions
