0f0a2a08690ceb533fa1b44f134f38ec2f0748ae
Saecki <tobiasschmitz2001@gmail.com>
2022-04-26 01:24:43 +0200

    feat: cache crate info

--- lua/crates/state.lua
+++ lua/crates/state.lua
@@ -5,16 +5,19 @@ local State = {}
 
 
 
+
 local config = require("crates.config")
 local Config = config.Config
 local toml = require("crates.toml")
 local Crate = toml.Crate
 local types = require("crates.types")
+local CrateInfo = types.CrateInfo
 local Version = types.Version
 
 State.cfg = {}
 State.vers_cache = {}
 State.crate_cache = {}
+State.info_cache = {}
 State.visible = true
 
 return State
