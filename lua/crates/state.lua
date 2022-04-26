a6c3fc9c7e8703ce6199535340ebc8d81fc22696
Saecki <tobiasschmitz2001@gmail.com>
2022-04-26 13:15:06 +0200

    feat: cache diagnostics

--- lua/crates/state.lua
+++ lua/crates/state.lua
@@ -6,18 +6,21 @@ local State = {}
 
 
 
+
 local config = require("crates.config")
 local Config = config.Config
 local toml = require("crates.toml")
 local Crate = toml.Crate
 local types = require("crates.types")
 local CrateInfo = types.CrateInfo
+local Diagnostic = types.Diagnostic
 local Version = types.Version
 
 State.cfg = {}
 State.vers_cache = {}
 State.crate_cache = {}
 State.info_cache = {}
+State.diagnostic_cache = {}
 State.visible = true
 
 return State
