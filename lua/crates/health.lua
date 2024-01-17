ac059c6f31338d77a291cae721de6e4d69b92091
Saecki <tobiasschmitz2001@gmail.com>
2024-01-17 16:34:34 +0100

    chore: update health check

--- lua/crates/health.lua
+++ lua/crates/health.lua
@@ -4,12 +4,7 @@ local state = require("crates.state")
 local util = require("crates.util")
 
 function M.check()
-    vim.health.start("Checking required plugins")
-    if util.lualib_installed("plenary") then
-        vim.health.ok("plenary.nvim installed")
-    else
-        vim.health.error("plenary.nvim not found")
-    end
+    vim.health.start("Checking plugins")
     if util.lualib_installed("null-ls") then
         vim.health.ok("null-ls.nvim installed")
     else
